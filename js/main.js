8b3a1e304b791f5e3c8742be7e1ef85f1dff72a9
Александр <kalyghnii@gmail.com>
2020-07-20 01:37:55 +0500

    Задание 4.1

--- js/main.js
+++ js/main.js
@@ -43,6 +43,8 @@ var bigPhotoElementImage = bigPhotoElement.querySelector('.big-picture__img img'
 var bigPhotoElementLikes = bigPhotoElement.querySelector('.likes-count');
 var bigPhotoElementCommentsCounter = bigPhotoElement.querySelector('.comments-count');
 var bigPhotoElementCommentsText = bigPhotoElement.querySelector('.social__caption');
+var bigPhotoButtonClose = bigPhotoElement.querySelector('.big-picture__cancel');
+var bigPhotoInputComments = bigPhotoElement.querySelector('.social__footer-text');
 var effects = {
   none: {
     class: '',
@@ -142,6 +144,9 @@ var createPhotoElement = function (photo) {
   photoElement.querySelector('.picture__img').alt = photo.alt;
   photoElement.querySelector('.picture__likes').textContent = photo.likes;
   photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  photoElement.addEventListener('click', function () {
+    onOpenBigPhoto(photo);
+  });
   return photoElement;
 };
 
@@ -186,7 +191,11 @@ var disableScroll = function () {
   body.classList.add('modal-open');
 };
 
-var openBigPhoto = function (bigPhoto) {
+var enableScroll = function () {
+  body.classList.remove('modal-open');
+};
+
+var onOpenBigPhoto = function (bigPhoto) {
   bigPhotoElement.classList.remove('hidden');
   bigPhotoElementImage.src = bigPhoto.url;
   bigPhotoElementLikes.textContent = bigPhoto.likes;
@@ -194,6 +203,14 @@ var openBigPhoto = function (bigPhoto) {
   createCommentElements(bigPhoto.comments);
   bigPhotoElementCommentsText.textContent = bigPhoto.description;
   disableScroll();
+  document.addEventListener('keydown', onBigPhotoPressEsc);
+};
+
+
+var onCloseBigPhoto = function () {
+  bigPhotoElement.classList.add('hidden');
+  enableScroll();
+  document.removeEventListener('keydown', onBigPhotoPressEsc);
 };
 
 var hideCommentCounter = function () {
@@ -211,24 +228,34 @@ var applyPhotoScale = function (value) {
 
 var onChangeEffectImage = function (evt) {
   if (evt.target.classList.contains('effects__radio')) {
-    if (currentEffect !== effects.none) {
-      photoPreview.classList.remove(currentEffect.class);
-    }
-    currentEffect = effects[evt.target.value];
-    photoPreview.style.filter = currentEffect.changeIntensity(1);
-
-    if (currentEffect === effects.none) {
-      saturationScale.classList.add('hidden');
-    } else {
-      photoPreview.classList.add(currentEffect.class);
-      saturationScale.classList.remove('hidden');
-    }
+    applyEffect(effects[evt.target.value], 1);
   }
 };
+
+var applyEffect = function(effect, intensity) {
+  if (currentEffect !== effects.none) {
+    photoPreview.classList.remove(currentEffect.class);
+  }
+  currentEffect = effect;
+  photoPreview.style.filter = currentEffect.changeIntensity(intensity);
+
+  if (currentEffect === effects.none) {
+    saturationScale.classList.add('hidden');
+  } else {
+    photoPreview.classList.add(currentEffect.class);
+    saturationScale.classList.remove('hidden');
+  }
+}
 
 var onDocumentKeydown = function (evt) {
   if (evt.key === 'Escape') {
     closeEditor();
+  }
+};
+
+var onBigPhotoPressEsc = function(evt) {
+  if (evt.key === 'Escape') {
+    onCloseBigPhoto();
   }
 };
 
@@ -244,18 +271,7 @@ var closeEditor = function () {
   body.classList.remove('modal-open');
   imageEditingForm.classList.add('hidden');
   document.removeEventListener('keydown', onDocumentKeydown);
-  effectsReload();
-};
-
-var effectsReload = function () {
-  photoPreview.style = {
-    filter: 'none'
-  };
-  photoPreview.classList.remove('effects__preview--chrome');
-  photoPreview.classList.remove('effects__preview--sepia');
-  photoPreview.classList.remove('effects__preview--marvin');
-  photoPreview.classList.remove('effects__preview--phobos');
-  photoPreview.classList.remove('effects__preview--heat');
+  applyEffect(effects.none, 0);
 };
 
 var decreasePhotoScale = function () {
@@ -316,6 +332,11 @@ var onPinMouseDown = function () {
   photoPreview.style.filter = currentEffect.changeIntensity(currnetSaturation);
 };
 
+var onPopapCancellationOfClosing = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+}
 
 controlFormOpen.addEventListener('change', function () {
   openEditor();
@@ -339,20 +360,19 @@ hashtagsInput.addEventListener('input', function () {
   validateHashtags();
 });
 
+hashtagsInput.addEventListener('keydown', onPopapCancellationOfClosing);
+
+bigPhotoInputComments.addEventListener('keydown', onPopapCancellationOfClosing);
+
 pinSaturationEffect.addEventListener('mouseup', function () {
   onPinMouseDown();
 });
-
 
 buttonSubmit.addEventListener('submit', function (evt) {
   evt.preventDefault();
 });
 
-pictureContainer.addEventListener('click', function (evt) {
-    openBigPhoto(photos[evt.target.alt]);
-    /* console.log(evt.target.alt); */
-
-});
+bigPhotoButtonClose.addEventListener('click', onCloseBigPhoto);
 
 photos = generatePhotos(PHOTOS_COUNT);
 createPhotoElements();
